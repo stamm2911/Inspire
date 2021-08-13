3cae20eeb2649a8a8711a7f4f20b5c638163f5bf
00anp <alfonso.nunez29@gmail.com>
2021-08-12 19:32:45 -0500

    Created React components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/home";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Marketpalce from "./components/Marketpalce";
+import User from "./components/User";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <Home />
+      <Marketpalce />
+      <User />
+      <Login />
+    </main>
   );
 }
-
-export default App;
