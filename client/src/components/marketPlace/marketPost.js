af8dd0677fd3f94e7227cc47c07899b43d7bb6c3
Eduardo Stamm <edstamm29@gmail.com>
2021-08-15 16:44:58 -0500

    localStorage implementation

--- client/src/components/marketPlace/marketPost.js
+++ client/src/components/marketPlace/marketPost.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useCartContext } from "../../utils/CartContext";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 function MarketPost({ products }) {
-  const {addItem} = useCartContext();
+  const { addItem } = useCartContext();
 
   if (!products.length) {
     return <h3>No Posts Yet</h3>;
@@ -11,22 +11,27 @@ function MarketPost({ products }) {
 
   return (
     <>
-      {products && products.map((product) => (
-        <article key={uuidv4()} className="market-post-container">
-          <img src={product.image} alt="pic" />
-          <section className="market-post-info">
-            <div>
-              <p>${product.price}</p>
-              <p>{product.productName}</p>
-              <p>{product.productDesc}</p>
-              <p>{product.available ? "Available" : "Not available"}</p>
-            </div>
-            <div>
-              <button onClick={() => addItem(product)}>Add</button>
-            </div>
-          </section>
-        </article>
-      ))}
+      {products &&
+        products.map((product) => (
+          <article key={uuidv4()} className="market-post-container">
+            <img src={product.image} alt="pic" />
+            <section className="market-post-info">
+              <div>
+                <p>${product.price}</p>
+                <p>{product.productName}</p>
+                <p>{product.productDesc}</p>
+                <p>{product.available ? "Available" : "Not available"}</p>
+              </div>
+              <div>
+                {product.available ? (
+                  <button onClick={() => addItem(product)}>Add</button>
+                ) : (
+                  <button disabled>Add</button>
+                )}
+              </div>
+            </section>
+          </article>
+        ))}
     </>
   );
 }
--- client/src/utils/CartContext.js
+++ client/src/utils/CartContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
@@ -7,6 +7,11 @@ export const useCartContext = () => useContext(CartContext);
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem("cart")))
+      setCart(JSON.parse(localStorage.getItem("cart")));
+  }, []);
+
   const addItem = (newItem) => {
     const newCart = [...cart, newItem];
     setCart(newCart);
@@ -14,7 +19,7 @@ export const CartProvider = ({ children }) => {
 
   const removeItem = (oldItem) => {
     console.log(oldItem);
-    oldItem.toDelete = 'delet';
+    oldItem.toDelete = "delet";
     console.log(oldItem);
     console.log(oldItem._id);
     const newCart = cart.filter((item) => {
@@ -23,6 +28,10 @@ export const CartProvider = ({ children }) => {
     console.log(newCart);
     setCart(newCart);
   };
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  });
 
   return (
     <CartContext.Provider value={{ cart, addItem, removeItem }}>
